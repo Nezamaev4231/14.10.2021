cc13c672cfcbfa4bfca9a2c233c7781b630244be
unknown <you@example.c>
2021-04-10 09:58:57 +0300

    10.04.2021

---
+++ 2 work/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_work
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.14 в
+
+            Console.WriteLine("Введите число");
+            double a = Convert.ToDouble(Console.ReadLine());
+            double a2 = Math.Pow("Ответ:" + a2);
+
+            //1.14 ж Сами
+            Console.WriteLine("1.14 ж");
+            Console.WriteLine("Введите число");
+            // Считываем число с консоли и конвертируем к числу
+            double b = Convert.ToDouble(Console.ReadLine());
+            double b2 =-7,5*Math.Pow (b, 2);
+            Console.WriteLine("Ответ:" + b2);
+
+            //1.14 м
+            Console.WriteLine("1.14 м");
+            Console.WriteLine("Введите число x");
+            Console.ReadKey();
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите число y");
+            double y = Convert.ToDouble(Console.ReadLine());
+            double xy = -5 * Math.Sqrt( + Math.Sqrt(y));
+            Console.WriteLine("" + xy);
+
+
+        }
+}
